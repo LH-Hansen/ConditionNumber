ae9d3b441d38de188a5369bd0c0c0f32262a7c41
Admin <Admin@DESKTOP-9TLL79V>
2023-05-26 12:57:04 +0200

    Completed the requirements of the task.

---
+++ ConditionNumber/Model/Kondital.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace ConditionNumber.Model;
+
+public class Kondital
+{
+    public double BeregnKondital(double mp, double hp)
+    {
+        double kondital = (mp / hp) * 15.3;
+
+        return kondital;
+    }
+
+    public double BeregnIltOptagelse(double konTal, double vægt)
+    {
+        double iltOptagelse = (konTal * vægt) / 1000;
+
+        return Math.Round(iltOptagelse, 1);
+    }
+}
--- ConditionNumber/Program.cs
+++ ConditionNumber/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace ConditionNumber;
+﻿using ConditionNumber.Model;
+
+namespace ConditionNumber;
 
 class Program
 {
     static void Main()
     {
+        Kondital myKondital = new Kondital();
 
+        while (true) 
+        {
+            Console.Write("Angiv køn\n0. Mand\n1. Kvinde\nVælg køn: ");
+            bool isFemale = Convert.ToBoolean(Convert.ToByte(Console.ReadLine()));
+
+            Console.Clear();
+
+            Console.Write("Indtast alder: ");
+            byte alder = Convert.ToByte(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Indtast vægt i kg: ");
+            double vægt = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Indtast maxpuls: ");
+            double maxPuls = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.Write("Indtast hvilepuls: ");
+            double hvilePuls = Convert.ToDouble(Console.ReadLine());
+
+            Console.Clear();
+
+            Console.WriteLine("Dit kondital er:        \t{0}\tml/kg/min", myKondital.BeregnKondital(maxPuls, hvilePuls));
+            Console.WriteLine("Din max iltoptagelse er:\t{0}\tl/ml", myKondital.BeregnIltOptagelse(myKondital.BeregnKondital(maxPuls, hvilePuls), vægt));
+        }
     }
 }
